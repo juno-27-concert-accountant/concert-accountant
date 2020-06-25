7d22aeaaa8777244020b6e69f2af0dbd8381fb7c
Angelica Chung <Angelica.Ramos.91@gmail.com>
2020-06-25 18:23:33 -0400

    started Axios call to shift through data (AC)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 // import firebase from './firebase';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+	componentDidMount() {
+		axios({
+			url: "https://app.ticketmaster.com/discovery/v2/events.json",
+			params: {
+				apikey: "Mh0RGGBfkgADAASrXM25WfhUueio9rgV",
+				type: "music",
+				
+			}
+		}).then(response => {
+			const res = response.data._embedded.events;
+			console.log(res)
+		})
+	}
+	
 	render() {
 		return (
 			<div className="App">
