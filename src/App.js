8d8af750d4660b271fb52246ef4513ce1bd41bfb
Angelica Chung <Angelica.Ramos.91@gmail.com>
2020-06-26 15:37:40 -0400

    created data obj from API call. I still need to go in and convert date to a number value, get ticket prices. Changed this.state.event.state to this.state.event.country as some results do not have a state (international) and it breaks the API call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 // import firebase from './firebase';
-import moment from 'moment';
 import './App.css';
 import axios from 'axios';
 
@@ -9,7 +8,7 @@ class App extends Component {
 		super();
 
 		this.state = {
-			
+			event: [],
 		};
 	}
 	
@@ -18,21 +17,95 @@ class App extends Component {
 			url: "https://app.ticketmaster.com/discovery/v2/events.json",
 			params: {
 				apikey: "Mh0RGGBfkgADAASrXM25WfhUueio9rgV",
-				type: "music",
-				name: "Drake"
+				// genre: "music",
+				// name: "Madonna"
 			}
 		}).then(response => {
 			const res = response.data._embedded.events;
-
-			moment().format();
 			console.log(res)
+			// Map over response to create obj with needed data
+			const resEvent = res.map( (data) => {
+				// To get ID
+				const eventID = data.id;
+				
+				// To get the name of the artist
+				const name = data.name;
+				const artist = data._embedded.attractions.map(art => {
+					return art.name
+				});
+				
+				
+				// To get the venue
+				const venue =  data._embedded.venues[0].name;
+
+				// To get city, state, and City, State combo
+				const city = data._embedded.venues[0].city.name;
+				const country = data._embedded.venues[0].country.name;
+				const cityCountry = `${city}, ${country}`;
+				
+				// To get Date
+				const dateStr = data.dates.start.localDate;
+				const dateConvert = d => {
+					const unixEpochTime = d;
+					const newDate = new Date(unixEpochTime);
+					const dateFull = new Date(newDate.setTime(newDate.getTime() + 1 * 86400000));
+
+					const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+					const dateMonth = dateFull.getMonth();
+					const dateDay = dateFull.getDate();
+
+					const date = `${monthNames[dateMonth]} ${dateDay}`;
+					console.log(date)
+					return date;
+				};
+
+				const dateNum = dateConvert(dateStr);
+
+
+				// To get image
+				const imgUrl = data.images[0].url;
+
+				// Link to purchase tickets
+				const tickets = data.url;
+
+				// To get price
+				const price = "";
+
+				// Return obj to push to this.state.event
+				return ({
+					eventID,
+					name,
+					artist,
+					venue,
+					location: {
+						city,
+						country,
+						cityCountry,
+					},
+					date: {
+						dateStr,
+						dateNum
+					},
+					imgUrl,
+					tickets,
+					price
+				})
+				
+			})
+
+			// Update state with obj
+			this.setState({
+				event: resEvent,
+			})
+
 		})
 	}
 	
 	render() {
 		return (
 			<div className="App">
-			
+
 			</div>
 		);
 	}
