2f4ea8056fed97883c9f505d0d30d754d29d57f5
Angelica Chung <Angelica.Ramos.91@gmail.com>
2020-06-26 13:49:17 -0400

    Update axios

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
 // import firebase from './firebase';
+import moment from 'moment';
 import './App.css';
 import axios from 'axios';
 
 class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			
+		};
+	}
+	
 	componentDidMount() {
 		axios({
 			url: "https://app.ticketmaster.com/discovery/v2/events.json",
 			params: {
 				apikey: "Mh0RGGBfkgADAASrXM25WfhUueio9rgV",
 				type: "music",
-				
+				name: "Drake"
 			}
 		}).then(response => {
 			const res = response.data._embedded.events;
+
+			moment().format();
 			console.log(res)
 		})
 	}
