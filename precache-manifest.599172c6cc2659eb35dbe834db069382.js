84d26bb68d3cf5b16cb5f90bfa991bb5aac7abd2
Angelica Chung <Angelica.Ramos.91@gmail.com>
2020-07-01 13:13:53 -0400

    Updates

---
+++ precache-manifest.599172c6cc2659eb35dbe834db069382.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60a60dbc73c57cc9f262871dca12ea54",
+    "url": "/concert-accountant/index.html"
+  },
+  {
+    "revision": "42e48246e54431b43a62",
+    "url": "/concert-accountant/static/css/main.11277e60.chunk.css"
+  },
+  {
+    "revision": "3ed7c8cb62baa313cb6a",
+    "url": "/concert-accountant/static/js/2.cedb7ca7.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/concert-accountant/static/js/2.cedb7ca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42e48246e54431b43a62",
+    "url": "/concert-accountant/static/js/main.1364a42c.chunk.js"
+  },
+  {
+    "revision": "d5ae42b7e146fb1a5d3f",
+    "url": "/concert-accountant/static/js/runtime-main.cc4cdc34.js"
+  }
+]);
