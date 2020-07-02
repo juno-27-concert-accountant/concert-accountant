d07626f3a09cd50845eeb166bc6f4fa72274b024
katmdev <katherinedevrieze@gmail.com>
2020-07-02 15:46:55 -0400

    form styling

--- src/Landing.js
+++ src/Landing.js
@@ -14,24 +14,29 @@ class Landing extends Component {
     render() {
         const {newName, newList, newBudget} = this.props.data
         return(
-            <Fragment>
-                <section>
-                    <h1>Concert Accountant</h1>
-                    <h2>Set your budget. Browse concerts you want to see. Add them to your list. Let us worry about the math.</h2>
-                </section>
-                <section>
-                    <form>
+                <div className="wrapper header__wrapper">
+                    <section className="header__left">
+                        <h1>Concert</h1>
+                        <h1>Accountant</h1>
+                        <h2>Set your budget.</h2>
+                        <h2>Browse concerts.</h2>
+                        <h2>Add them to your list.</h2>
+                        <h2>Let us worry about the math.</h2>
+                    </section>
+                    <section className="header__right">
                         <h4>Start budgeting now</h4>
-                        <label htmlFor="newName">Your name</label>
-                        <input onChange={this.props.handleInputChange}  name="newName" type="text" value={newName}/>
-                        <label htmlFor="newList">Your list</label>
-                        <input onChange={this.props.handleInputChange}  name="newList" type="text" value={newList}/>
-                        <label htmlFor="newBudget">Your budget</label>
-                        <input onChange={this.props.handleInputChange}  name="newBudget" type="text" value={newBudget}/>
-                        <button onClick={this.props.handleSubmit}>Start saving now</button>
-                    </form>
-                </section>
-            </Fragment>
+                        <hr/>
+                        <form className="header__form">
+                            <label htmlFor="newName">Your name</label>
+                            <input onChange={this.props.handleInputChange}  name="newName" type="text" value={newName}/>
+                            <label htmlFor="newList">Your list</label>
+                            <input onChange={this.props.handleInputChange}  name="newList" type="text" value={newList}/>
+                            <label htmlFor="newBudget">Your budget</label>
+                            <input onChange={this.props.handleInputChange}  name="newBudget" type="text" value={newBudget}/>
+                            <button onClick={this.props.handleSubmit}>Start now</button>
+                        </form>
+                    </section>
+                </div>
         )
     }
 }
