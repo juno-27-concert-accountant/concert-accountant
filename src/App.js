bab1e04c35533b850da9f6cdf57a7d92bfbcb936
Oliver Kersey <oliverjkersey@gmail.com>
2020-07-02 13:30:29 -0400

    same problem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,4 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import './App.css';
-import firebase from './firebase';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import axios from 'axios';
-import Login from './Login';
-import Dashboard from './Dashboard';
-=======
 import "./App.css";
 import firebase from './firebase';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
@@ -18,7 +10,6 @@ import Search from './Search.js';
 import ConcertCard from './ConcertCard.js';
 import Landing from './Landing.js'
 
->>>>>>> 135b53005e4759c6af1a9d75b62e0bdf42219928
 
 
 class App extends Component {
@@ -35,15 +26,8 @@ class App extends Component {
 
 					{/* <Route exact path="/event/" component={ConcertCard} /> */}
 
-<<<<<<< HEAD
-				
-			<Router>
-				<div className="App">
-					<Dashboard />
-=======
 					{/* Show concert details */}
 			<Route exact path="/event/:eventID" component={ConcertDetailsPopUp} />
->>>>>>> 135b53005e4759c6af1a9d75b62e0bdf42219928
 				</div>
 			</Router>
 		);
