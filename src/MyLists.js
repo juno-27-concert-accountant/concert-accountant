47aa3c885b8abcbee0c586be4f6e4829940fbdba
Oliver Kersey <oliverjkersey@gmail.com>
2020-07-03 01:29:03 -0400

    added functinoality to publish list function

--- src/MyLists.js
+++ src/MyLists.js
@@ -8,17 +8,29 @@ class MyLists extends Component {
   constructor() {
       super();
       this.state = {
-      	  newName: '',
-          newList: '',
-          newBudget: '',
-			    published: false,
-		    	userSessionKey: ''
+        listDetails: {},
+
       }
   }
+
+  // dbRef.push(userInfo)
+
+//    // Get a key for a new Post.
+//    var newPostKey = firebase.database().ref().child('posts').push().key;
+
+//    // Write the new post's data simultaneously in the posts list and the user's post list.
+//    var updates = {};
+//    updates['/posts/' + newPostKey] = postData;
+//    updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+ 
+//    return firebase.database().ref().update(updates);
+//  }
+ 
 
   // https://console.firebase.google.com/project/concert-accountant-cca1f/database/concert-accountant-cca1f/data/-MBHHUuah2kyAgF_s_2d
 
 
+  
   componentDidMount() {
     // const sessionID = '-MBHHUuah2kyAgF_s_2d';
     const sessionID = this.props.id;
@@ -27,7 +39,10 @@ class MyLists extends Component {
     // console.log(dbRef);
     dbRef.on('value', (response) => {
       const data = response.val();
-      console.log(data);
+      // console.log(data);
+      this.setState({ 
+        listDetails: data,
+      })
       // console.log(this.props.id);
       // for(key in data) {
         // this.state 
@@ -40,33 +55,69 @@ class MyLists extends Component {
     // The key is 
     // -MBGveceho8kTcigOPXt
     //How do I do that?
-
+    
+    //onClick=publishList
+    //prevent default
+  //
   
-
   //find key related to list
-
+  
   //use a for in loop to grab the key
-
+  
   //firebase.databse().ref(`)
+  
+  handlePublish = (e) => {
+    e.preventDefault();
+    const list = e.target.name;
+    const sessionID = this.props.id;
+    const dbRef = firebase.database().ref(`/${sessionID}`).child(list);
+    const updateObject = {
+    published: true,
+
+};
+
+    dbRef.update(updateObject);
+    // dbRef.on('value', (response) => {
+    //   const data = response.val();
+    //   console.log(data);
+    //   data.update('true');
+    // });
+    
+    // this.setState({
+    //   [list]: !published,
+    //   // {
+    //   //   published: true,
+    //   // }
+    // })
+  }
+
+
 
   render() {
-
-
+    const {listDetails} = this.state; 
+    const wishlist = listDetails.wishlist;
     // console.log(this.props)
     return(
-        <div className="profileDiv">
-          <h2>Music Lists</h2>
+      <div className="profileDiv">
+        <h2>{listDetails.name}'s List</h2>
+        <ul>
+          <li>
+            {/* {listDetails[wishlist].name} */}
+            {console.log(wishlist.value)}
+          </li>
+        </ul>
+      <button onClick={this.handlePublish} name="wishlist">Publish</button>
         <h3><a href="#" className="profilePopup">Some Music Lists</a></h3>
-        <h3>Wish Lists\</h3>
+        <h3>Wish Lists\</h3>1
         <h3>Budget List</h3>
         </div>
 
     );
     }
-}
-
-export default MyLists;
-
+  }
+  
+  export default MyLists;
+  
 // export default LocalLists;
 
 // import React, {Component} from 'react';
